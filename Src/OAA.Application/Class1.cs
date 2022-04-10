f2a9616369d5561a1ba2ee70189cd09032aad2b7
Hüseyin <huseyin.kayakiran@gmail.com>
2022-04-10 04:52:44 +0300

    Init
    Layers added.

---
+++ Src/OAA.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OAA.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Src/OAA.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OAA.Domain
+{
+    public class Class1
+    {
+    }
+}
